d904c351cd80827ab442dba93523d5a846c1e4a0
Thomas <tmayr1099@gmail.com>
2025-04-26 15:52:05 +0200

    feat: add welcome screen

--- pizza_app/lib/app_view.dart
+++ pizza_app/lib/app_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:pizza_app/screens/auth/views/welcome_screen.dart';
+import 'package:pizza_app/screens/home/views/home_screen.dart';
 
 class MainAppView extends StatelessWidget {
   const MainAppView({super.key});
@@ -7,6 +11,24 @@ class MainAppView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Pizza Delivery",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          surface: Colors.grey.shade100,
+          onSurface: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white
+        )
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: ((context, state) {
+          if(state.status == AuthenticationStatus.authenticated) {
+            return const HomeScreen();
+          } else {
+            return const WelcomeScreen();
+          }
+        }),
+      ),
     );
   }
 }
