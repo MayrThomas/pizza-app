c9ce68b12dfd0c5a2cdc2a709aaf1c9c5033ba53
Thomas <tmayr1099@gmail.com>
2025-04-26 14:49:11 +0200

    feat: add AuthenticationBloc

---
+++ pizza_app/lib/app_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainAppView extends StatelessWidget {
+  const MainAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Pizza Delivery",
+    );
+  }
+}
