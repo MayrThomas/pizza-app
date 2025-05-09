f5ed946e7752ec904fe982164d0e94effe11c0c6
Thomas <tmayr1099@gmail.com>
2025-05-09 16:49:06 +0200

    feat: home screen basic design

--- pizza_app/lib/screens/home/views/home_screen.dart
+++ pizza_app/lib/screens/home/views/home_screen.dart
@@ -1,10 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:pizza_app/screens/auth/blocs/sign_in/sign_in_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/8.png',
+                scale: 14,
+              ),
+              SizedBox(width: 10),
+              const Text(
+                'PIZZA',
+                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(MdiIcons.cart)),
+            IconButton(
+                onPressed: () {
+                  context.read<SignInBloc>().add(SignOutRequired());
+                },
+                icon: Icon(MdiIcons.logout))
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 9 / 16
+              ),
+              itemCount: 6,
+              itemBuilder: (context, int i) {
+                return Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.shade300,
+                            blurRadius: 5,
+                            offset: Offset(3, 3)
+                        )
+                      ]
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/${i + 1}.png',
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(30)
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 4,
+                                    horizontal: 8
+                                ),
+                                child: Text(
+                                  "NON-VEG",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 10
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 8.0),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.green.withValues(alpha: 0.2),
+                                  borderRadius: BorderRadius.circular(30)
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 4,
+                                    horizontal: 8
+                                ),
+                                child: Text(
+                                  "🌶️ BALANCED",
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 10
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      const Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Pizza Nr X",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "A very nice Pizza, you decide for yourself",
+                          style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.grey
+                          ),
+                        ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          child: Row(
+                            children: [
+                              Text(
+                                "10 €",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Theme.of(context).colorScheme.primary,
+                                  fontWeight: FontWeight.w700
+                                ),
+                              ),
+                              SizedBox(width: 5,),
+                              const Text(
+                                "12 €",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.w700,
+                                  decoration: TextDecoration.lineThrough,
+                                  decorationColor: Colors.grey
+                                ),
+                              ),
+                            ],
+                          )
+                      )
+                    ],
+                  ),
+                );
+              }),
+        ));
   }
 }
